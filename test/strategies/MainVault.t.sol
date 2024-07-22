6a3c536d825bf7fe23088b107095b332f850ca53
Ned Albo <ned.albo@mailfence.com>
2024-07-22 14:31:12 +0200

    added dep on tokenized-strategy

--- test/strategies/MainVault.t.sol
+++ test/strategies/MainVault.t.sol
@@ -4,11 +4,7 @@ pragma solidity ^0.8.0;
 import "@test/BaseTest.t.sol";
 
 contract MainVaultTest is BaseTest {
-
-    public 
-
     function setUp() public virtual override {
         super.setUp();
-
     }
 }
